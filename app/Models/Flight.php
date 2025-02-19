11777b9af3f96450fd120fc7751a4b53220ce2d4
matias-stadler <koutarcj@gmail.com>
2025-02-19 11:37:25 +0100

    fix:models

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Flight extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        "date",
+        "departure",
+        "arrival",
+        "aircraft_id",
+        "available"
+    ];
+
+    public function aircraft(): BelongsTo
+    {
+        return $this->belongsTo(Aircraft::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, "flight_user");
+    }
 }
