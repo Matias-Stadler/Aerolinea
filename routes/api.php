a9cc58109db1fa1f3949a91180f6d0aab344af24
matias-stadler <koutarcj@gmail.com>
2025-02-19 12:12:58 +0100

    fix:added routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\FlightController;
+use App\Http\Controllers\api\AircraftController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post("/aircraft", [AircraftController::class, "store"])->name("apiaircraftstore");
+Route::get("/aircraft", [AircraftController::class, "index"])->name("apiaircraftindex");
+Route::get("/aircraft/{id}", [AircraftController::class, "show"])->name("apiaircraftshow");
+Route::put("/aircraft/{id}", [AircraftController::class, "update"])->name("apiaircraftupdate");
+Route::delete("/aircraft/{id}", [AircraftController::class, "destroy"])->name("apiaircraftdestroy");
+
+Route::post("/flights", [FlightController::class, "store"])->name("apiflightstore");
+Route::get("/flights", [FlightController::class, "index"])->name("apiflightindex");
+Route::get("/flights/{id}", [FlightController::class, "show"])->name("apiflightshow");
+Route::put("/flights/{id}", [FlightController::class, "update"])->name("apiflightupdate");
+Route::delete("/flights/{id}", [FlightController::class, "destroy"])->name("apiflightdestroy");
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\BookingAllowed;
+use App\Http\Controllers\FlightController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FlightController::class, "index"])->name("index");
+Route::get("/flights/{id}", [FlightController::class, "show"])->name("show")->middleware(BookingAllowed::class);
 
 Auth::routes();
 
